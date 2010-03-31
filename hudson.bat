d5966cdaaabfc4adc45ab88dc13852e36b86cdc4
NQH <NQH@dev.java.net>
2010-03-31 14:39:26 +0000

    DELETE: this class is not relevant for Cpptest

--- hudson.bat
+++ hudson.bat
@@ -9,8 +9,9 @@ REM  Version: 1.0
 REM
 REM  Description: Simple script for compiling cpptest plugin and start hudson with it
 REM ***************************************
-
 rmdir /S /Q "%HOME%\.hudson\plugins\cpptest" 
-call mvn package
+d:
+cd D:\checkout\hudson\trunk\plugins\cpptest
+call mvn -o package
 copy ".\target\cpptest.hpi" "%HOME%\.hudson\plugins\"
 java -jar "%HOME%\.hudson\hudson.war"

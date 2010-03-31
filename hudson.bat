ccb43cde754e51b1c43138c51830d618edccf5e1
NQH <NQH@dev.java.net>
2010-03-31 14:50:42 +0000

    FIX: ... #$%^@ windows  :)

--- hudson.bat
+++ hudson.bat
@@ -9,9 +9,9 @@ REM  Version: 1.0
 REM
 REM  Description: Simple script for compiling cpptest plugin and start hudson with it
 REM ***************************************
-rmdir /S /Q "%HOMEPATH%\.hudson\plugins\cpptest" 
+rmdir /S /Q "%HOMEDRIVE%%HOMEPATH%\.hudson\plugins\cpptest" 
 d:
 cd D:\checkout\hudson\trunk\plugins\cpptest
 call mvn -o package
-copy ".\target\cpptest.hpi" "%HOMEPATH%\.hudson\plugins\"
-java -jar "%HOMEPATH%\.hudson\hudson.war"
+copy ".\target\cpptest.hpi" "%HOMEDRIVE%%HOMEPATH%\.hudson\plugins\"
+java -jar "%HOMEDRIVE%%HOMEPATH%\.hudson\hudson.war"
